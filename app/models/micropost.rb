5771b214f5e9469c94acd2af8aae1414855d8c96
fancol <ngocthoson1@gmail.com>
2020-05-11 03:22:21 -0700

    Finish and deploy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,12 +1,7 @@
 class Micropost < ApplicationRecord
     belongs_to :user
-<<<<<<< HEAD
     validates :content, length: {maximum: 140},
                         presence: true
 
-=======
-  validates :content, length: { maximum: 140 }
-                    presence :true
-    
->>>>>>> finally
+
 end
